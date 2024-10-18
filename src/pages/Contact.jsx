9e2c24b68eb3943ff0d1e2e3538f669538eabb77
Ajay Mondal <mrakmondal6612@gmail.com>
2024-10-18 17:45:49 +0530

    contact update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -24,16 +24,16 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
     setCurrentAnimation("hit");
-    
+
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Ajay Mondal",
           from_email: form.email,
-          to_email: "mrakmondal6612@gmail.com",
+          to_email: "mr.aktv2529@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
@@ -43,7 +43,7 @@ const Contact = () => {
           setLoading(false);
           showAlert({
             show: true,
-            text: "Thank you for your message 😃",
+            text: `Thank you for your message 😃`,
             type: "success",
           });
 
